a2e4b2f3716752de291c16d0f5ad66a7fd683fda
Adarsh Solanki <adarsh.solanki@gmail.com>
2013-04-09 02:27:03 -0400

    minor additions, working on functions

--- src/Life.hs
+++ src/Life.hs
@@ -8,6 +8,14 @@ import Control.Monad
 -- board for the game is a 2d-array of Bool's
 type Board = [[Bool]]
 data Game = Game {
-	gameBoard :: Board,
-	
+	gameBoard  :: Board,
+	iterations :: Integer
 }
+
+numberOfNeighbors :: Game -> (Int, Int) -> Int
+numberOfNeighbors = 
+
+surrounding :: Game -> (Int, Int) -> [Bool]
+surrounding g (x, y) = case (x, y) of
+	(0, 0) 		-> (board !! y) !! x)
+
