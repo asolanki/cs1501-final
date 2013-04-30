693e045d4d506b0e23498b381ba86ed7840e9c75
Adarsh Solanki <adarsh.solanki@gmail.com>
2013-04-30 14:17:35 -0400

    step function completed

--- src/Life.hs
+++ src/Life.hs
@@ -1,21 +1,52 @@
+-- | A console implementation of Conway's Game of Life in Haskell
 module Life where
 
 import Data.List
-import Control.Monad
--- import Conrol.Concurrent (threadDelay)
--- import System.Console.ANSI (clearscreen)
 
--- board for the game is a 2d-array of Bool's
-type Board = [[Bool]]
-data Game = Game {
-	gameBoard  :: Board,
-	iterations :: Integer
-}
+-- |Cell type is given by a 2-tuple of integers (x, y)
+type Cell = (Int, Int)
+-- |Board is the current world of the game, given as a list of living cells
+type Board = [Cell]
 
-numberOfNeighbors :: Game -> (Int, Int) -> Int
-numberOfNeighbors = 
+-- |given a board, returns a list of 2-tuples made of each living cell
+--    and its number of neighbors
+determineNeighbors :: Board         -- ^ The current board
+                   -> [(Cell, Int)] -- ^ A list of 2-tuples containing each
+                                    --    living cell and its neighbor count
+ 
+determineNeighbors board = map (\xs)
+  where allNeighbors = concatMap (\(x,y) -> map (\(a,b) -> (x+a,y+b)) deltas) board
+        deltas = [ (-1,-1), (0,-1), (1,-1), -- the possible deltas for all
+                   (-1, 0),       , (1, 0), -- possible neighbor cell
+                   (-1, 1), (0, 1), (1, 1)] -- positions in the form (dx,dy)
+-- |step returns the next iteration of the given board
+step :: Board   -- ^ The current board
+     -> Board   -- ^ The next board
+step b = [c | (c,n) <- determineNeighbors b, alive (c,n)]
+  where alive  = newborn || survivor
+        newborn  ((x,y), n) = notelem (x,y) b && n == 3
+        survivor ((x,y), n) = elem (x,y) b && elem n [2,3]
 
-surrounding :: Game -> (Int, Int) -> [Bool]
-surrounding g (0, 0) = {- some fn -} ([1, 0], [0, 1], [1,1])
-surrounding g ()
 
+
+
+
+
+
+
+{-
+
+  -- board for the game is a 20x20 2d-array of Bool's
+  type Board = [[Bool]]
+  data Game = Game {
+  	gameBoard  :: Board,
+  	iterations :: Integer
+  }
+
+
+  numberOfNeighbors :: Game -> (Int, Int) -> Int
+  numberOfNeighbors g (x,y) = length surrounding g (x,y)
+
+  surrounding :: Game -> (Int, Int) -> [Bool]
+
+-}
